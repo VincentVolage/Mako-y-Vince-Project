727c686cf042908eeca5e2020153bffd316f521b
Maqueaux <mako.pegart@yahoo.com>
2024-10-15 15:56:27 +0200

    revert to main

--- Program.cs
+++ Program.cs
@@ -7,26 +7,10 @@ namespace Mako_y_Vince_Project
         static void Main(string[] args)
         {
             Console.WriteLine("slt");
-            Console.ReadLine(); #yeeeeeeeeeeee
+            Console.ReadLine();
             Console.ReadLine();
             Console.WriteLine("t'as pas réagi à ma réf sur Spirit");
             Console.ReadLine();
-            Console.WriteLine("C'était la musique");
-            Console.ReadLine();
-            Yo();
-        }
-
-        static void Yo()
-        {
-            Console.WriteLine("tfk ?");
-                string reponse = Console.ReadLine();
-
-            if(reponse == "r")
-            {
-                Console.WriteLine("HAHHAAHAHAHAHAAH T_T")
-            }
-            Console.ReadLine();
         }
     }
-
 }
