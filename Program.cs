0e9a690f170b4205c6aaa9d4e70949c8c7e5f38d
Maqueaux <mako.pegart@yahoo.com>
2024-10-15 15:57:12 +0200

    Mise à jour programme

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Mako_y_Vince_Project
 {
@@ -11,11 +7,26 @@ namespace Mako_y_Vince_Project
         static void Main(string[] args)
         {
             Console.WriteLine("slt");
-            Console.ReadLine(); #yeeeeeeeeeeee
+            Console.ReadLine();
             Console.ReadLine();
             Console.WriteLine("t'as pas réagi à ma réf sur Spirit");
             Console.ReadLine();
 
+            Console.WriteLine("C'était la musique");
+            Console.ReadLine();
+            Yo();
+        }
+
+        static void Yo()
+        {
+            Console.WriteLine("tfk ?");
+            string reponse = Console.ReadLine();
+
+            if (reponse == "r")
+            {
+                Console.WriteLine("HAHHAAHAHAHAHAAH T_T");
+            }
+            Console.ReadLine();
         }
     }
 }
